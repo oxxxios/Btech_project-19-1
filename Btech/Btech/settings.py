8a08426a9e527d0bdc528e79cc8ae03bd5d2263a
Bakdoolot A <altybaevbaku15@gmail.com>
2022-11-15 23:08:44 +0600

    Update Installed_apps

--- Btech/Btech/settings.py
+++ Btech/Btech/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Btech.apps.Btech19Config'
+    'Btech'
 ]
 
 MIDDLEWARE = [
