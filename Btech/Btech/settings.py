3f3be5ab74969ea11109f1a2929a25eb48858c11
Marrlenns <marlenormonbaev@gmail.com>
2022-11-15 23:40:15 +0600

    add serializers

--- Btech/Btech/settings.py
+++ Btech/Btech/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Btech19'
+    'Btech19',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'users'
 ]
 
 MIDDLEWARE = [
