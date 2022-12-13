9e25c839c3d40688ef55f5bf8e7ef7b1bfb3fe93
Marllenns <marlenormonbaev@gmail.com>
2022-12-13 23:52:57 +0600

    added abstractuser

--- Btech/users/views.py
+++ Btech/users/views.py
@@ -1,27 +1,5 @@
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import render
+from .serializers import SignUpSerializer
+from rest_framework import generics, status
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from .serializers import UserLoginValidateSerializer, UserCreateSerializer
-from django.contrib.auth.models import User
-
-
-@api_view(['POST'])
-def register_(request):
-    serializer = UserCreateSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    User.objects.create_user(**serializer.validated_data)
-    return Response(status=status.HTTP_201_CREATED, data={"message": "User created successfully"})
-
-
-@api_view(['POST'])
-def login_(request):
-    serializer = UserLoginValidateSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = authenticate(**serializer.validated_data)
-    if user:
-        Token.objects.filter(user=user).delete()
-        token = Token.objects.create(user=user)
-        return Response(data={'key': token.key})
-    return Response(status=status.HTTP_401_UNAUTHORIZED)
+from rest_framework.request import Request
