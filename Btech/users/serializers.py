2ba62f47dede7e585c88d111779df4cce34897be
Marllenns <marlenormonbaev@gmail.com>
2022-12-14 00:51:10 +0600

    FINISHED JWT AUTH

--- Btech/users/serializers.py
+++ Btech/users/serializers.py
@@ -23,3 +23,21 @@ class SignUpSerializer(serializers.ModelSerializer):
             raise ValidationError("Number must contain only digits!")
 
         return phone
+
+
+    def create(self, *args, **kwargs):
+        user = User(
+            username=self.validated_data['username'],
+            phone=self.validated_data['phone'],
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name']
+        )
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password2 != password:
+            raise ValidationError({password: "Passwords not match"})
+
+        user.set_password(password)
+        user.save()
+        return user
--- Btech/users/urls.py
+++ Btech/users/urls.py
@@ -1,7 +1,15 @@
+from views import SignUpView
 from django.urls import path
-from users.views import *
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
-urlpatterns = [
-  path('login/', login_),
-  path('register/', register_)
+urlpatterns=[
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('jwt/create', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+    path('jwt/verify', TokenVerifyView.as_view(), name='token-verify')
 ]
