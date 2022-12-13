76926ace91c6751ecb703e5bd695d17331fe1a37
Marllenns <marlenormonbaev@gmail.com>
2022-12-13 23:52:57 +0600

    added abstractuser

--- Btech/users/models.py
+++ Btech/users/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, phone, first_name, last_name, password, **extra_fields):
+#         user = self.model(
+#             phone=phone,
+#             first_name=first_name,
+#             last_name=last_name,
+#             **extra_fields
+#         )
+#
+#         user.set_password(password)
+#
+#         user.save()
+#
+#         return user
+#
+#     def create_superuser(self, username, phone, first_name, last_name, password, **extra_fields):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
+#
+#         if extra_fields.get("is_staff") is not True:
+#             raise ValueError("Superuser has to have is_staff being True")
+#
+#         if extra_fields.get("is_superuser") is not True:
+#             raise ValueError("Superuser has to have is_superuser being True")
+#
+#         return self.create_user(
+#             username=username,
+#             phone=phone,
+#             first_name=first_name,
+#             last_name=last_name,
+#             password=password,
+#             **extra_fields
+#         )
+class User(AbstractUser):
+    username=models.CharField(max_length=100, unique=True)
+    phone=models.CharField(max_length=13, unique=True)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = "first_name last_name".split()
+    def __str__(self):
+        return self.username
--- Btech/users/serializers.py
+++ Btech/users/serializers.py
@@ -1,18 +1,25 @@
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from rest_framework.serializers import ValidationError
 
+from .models import User
 
-class UserCreateSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+class SignUpSerializer(serializers.ModelSerializer):
+    phone = serializers.CharField(max_length=13)
+    first_name = serializers.CharField(min_length=100)
+    last_name = serializers.CharField(min_length=100)
+    password = serializers.CharField(min_length=8, write_only=True)
+    password2 = serializers.CharField(min_length=8, write_only=True)
 
-    def validate_username(self, username):
-        if User.objects.filter(username=username):
-            raise ValidationError('User already exists')
-        return username
+    class Meta:
+        model = User
+        fields = 'username phone first_name last_name password password2'.split()
 
+    def validate_phone(self, phone):
+        if len(phone) < 13:
+            raise ValidationError("Not enough numbers!")
+        elif phone[:4] != '+996':
+            raise ValidationError("Number must start with +996!")
+        elif not phone[1:].isdigit():
+            raise ValidationError("Number must contain only digits!")
 
-class UserLoginValidateSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+        return phone
